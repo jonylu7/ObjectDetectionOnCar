bca6ace3aa2d47486e7a222c57f91a3367fdd16e
p_03 <jonylu7@gmail.com>
2024-05-29 14:51:13 +0800

    t

--- data_ultr.py
+++ data_ultr.py
@@ -1,6 +1,6 @@
 from roboflow import Roboflow
 
 rf = Roboflow(api_key="YXeXgzKhCVUNBnZhK4KH")
-project = rf.workspace("vehicle-counter").project("vehicle-counter-5zkt5")
-version = project.version(2)
+project = rf.workspace("manas-mtp").project("mtp-vehicles")
+version = project.version(27)
 dataset = version.download("yolov5")
--- train.py
+++ train.py
@@ -1,21 +1,23 @@
-import torch
-import os
-import numpy as np
-from tqdm import tqdm
-from datetime import datetime
-from torch.utils.tensorboard import SummaryWriter
-from torch.utils.data import DataLoader
-from torchvision import transforms
-from data import BoundingBoxDataset
-from loss import SumSquaredErrorLoss
-from model import *
 from ultralytics import YOLO
+
+def loadVideoAndExport():
+    print()
+
+def valBest():
+    model=YOLO("model/yolov5m_map50_912.pt")
+    metrics = model.val()
+    print(metrics.box.map50)
+
+def train():
+    model = YOLO("model/yolov5m.pt")
+
+    results = model.train(data="data.yaml", epochs=100, imgsz=860, device="cuda", save=True,
+                          plots=True)
 
 if __name__ == '__main__':  # Prevent recursive subprocess creation
 
     # model = YOLOv1ResNet().to(device)
     # jonylu7: load pretrained and newest yolo model
-    model = YOLO("model/yolov5s.pt")
 
-    results = model.train(data="data.yaml", epochs=100, imgsz=640, device="mps", save=True,
-                          plots=True)
+    #train()
+    valBest()
