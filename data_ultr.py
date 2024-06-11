48e4c5faf770556d4468c11dff407ebbb7f09442
p_03 <jonylu7@gmail.com>
2024-06-11 14:28:24 +0800

    yolov5 run

--- data_ultr.py
+++ data_ultr.py
@@ -1,6 +1,6 @@
 from roboflow import Roboflow
-
 rf = Roboflow(api_key="YXeXgzKhCVUNBnZhK4KH")
-project = rf.workspace("manas-mtp").project("mtp-vehicles")
-version = project.version(27)
+project = rf.workspace("t-2jhay").project("tw-traffic-sign")
+version = project.version(5)
 dataset = version.download("yolov5")
+
--- train.py
+++ train.py
@@ -1,58 +1,24 @@
 from ultralytics import YOLO
 import cv2
+import torch
 
-
-def loadVideoAndExport(sorcefilepath, exportfilepath, frame_width=1280, frame_height=720):
-    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-    out = cv2.VideoWriter(exportfilepath, fourcc, 20, (frame_width, frame_height))
-    # Load the YOLOv8 model
-    model = YOLO("model/yolov5m_map50_912.pt")
-
-    # Open the video file
-    cap = cv2.VideoCapture(sorcefilepath)
-
-    # Loop through the video frames
-    while cap.isOpened():
-        # Read a frame from the video
-        success, frame = cap.read()
-
-        if success:
-            # Run YOLOv8 inference on the frame
-            results = model(frame)
-
-            # Visualize the results on the frame
-            annotated_frame = results[0].plot()
-
-            # Display the annotated frame
-            out.write(annotated_frame)
-
-            # Break the loop if 'q' is pressed
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                break
-        else:
-            # Break the loop if the end of the video is reached
-            break
-
-    # Release the video capture object and close the display window
-    cap.release()
-    cv2.destroyAllWindows()
 
 
 def valBest():
-    model = YOLO("model/yolov5m_map50_912.pt")
+    model=YOLO("model/yolov5n_best.pt")
     metrics = model.val()
     print(metrics.box.map50)
 
-
 def train():
-    model = YOLO("model/yolov5m.pt")
-
-    results = model.train(data="data.yaml", epochs=100, imgsz=860, device="cuda", save=True,
+    model = YOLO("model/yolov5n.pt")
+    torch.cuda.empty_cache()
+    model.train(data="data.yaml", epochs=100, imgsz=860, device="cuda:1", save=True,
                           plots=True)
-
 
 if __name__ == '__main__':  # Prevent recursive subprocess creation
 
-    # train()
-    # valBest()
-    loadVideoAndExport("dashcam/dashcam-2.mp4", "result_videos/dcam_result-2.mp4", 1280, 720)
+    # model = YOLOv1ResNet().to(device)
+    # jonylu7: load pretrained and newest yolo model
+
+    #train()
+    valBest()
