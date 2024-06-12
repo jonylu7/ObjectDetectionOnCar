85d1901a8c289c112dc572460bee3663e4ff5ac7
jonylu7 <jonylu7@gmail.com>
2024-06-12 17:17:15 +0800

    something

--- train.py
+++ train.py
@@ -3,22 +3,24 @@ import cv2
 import torch
 
 
-
 def valBest():
-    model=YOLO("model/yolov5n_best.pt")
-    metrics = model.val()
-    print(metrics.box.map50)
+    model = YOLO("model/yolov5n_best.pt")
+    model.export(format="engine")
+    # metrics = model.val()
+    # print(metrics.box.map50)
+
 
 def train():
     model = YOLO("model/yolov5n.pt")
-    torch.cuda.empty_cache()
+    # torch.cuda.empty_cache()
     model.train(data="data.yaml", epochs=100, imgsz=860, device="cuda:1", save=True,
-                          plots=True)
+                plots=True)
+
 
 if __name__ == '__main__':  # Prevent recursive subprocess creation
 
     # model = YOLOv1ResNet().to(device)
     # jonylu7: load pretrained and newest yolo model
 
-    #train()
+    # train()
     valBest()
